b121ee3974b2af4eb7ebdc1ae9bae519fb924596
josssss07 <joshuadesai03@gmail.com>
2025-11-06 19:19:52 -0500

    setup for client.py and server.py for fips203

---
+++ client.py
@@ -0,0 +1,6 @@
+'''
+    client.py will run on the client device, this will generate the public and private key pair. 
+    the public key will be sent to the aws ec2 isntance 
+    the performance metrics will be stored in a csv file and then the average will be taken. 
+    this can be compared against traditional algorithms like rsa 
+'''
