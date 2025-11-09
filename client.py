f754035ab1592ccf1aa1fb3407b3dfcdc83c7158
josssss07 <joshuadesai03@gmail.com>
2025-11-08 20:20:30 -0500

    encryption and decryption done, timers left

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 '''
-client side code
+    client side code
 '''
 
 #imports: 
@@ -18,7 +18,7 @@ warnings.filterwarnings("ignore")
 
 #configs 
 SERVER = "https://aws_goes_here_later:5000" #fips203 will run on port 5000 and rsa will run on port 5001 (read info.txt)
-ITERATION = 100 #api will be called 100 times when program is run to store data into our csv file. can be changed accordingly 
+ITERATION = 10 #api will be called 100 times when program is run to store data into our csv file. can be changed accordingly 
 CSV_FILE = 'benchnmark_results.csv' 
 
 #helper b64 encode and decode functions: 
@@ -32,3 +32,83 @@ def b64_e(x):
 ALG = next((algo for algo in oqs.get_enabled_kem_mechanisms() if "KYBER" in algo.upper() or "ML-KEM" in algo.upper()), None)
 if not ALG:
     raise SystemExit("No ML-KEM/Kyber implementation found in liboqs")
+
+#make a local keystore for the keys 
+KEYSTORE_FILE = "client_keystore.bin"
+KEYSTORE_KEY = "keystore_key.bin"
+
+if not os.path.exists(KEYSTORE_FILE):
+    print("generating new private key.....")
+    with oqs.KeyEncapsulation(ALG) as kem: 
+        kem_public_key = kem.generate_keypair()
+        private_key = kem.export_secret_key()
+
+    local_key = Fernet.generate_key()
+    with open(KEYSTORE_FILE, "wb") as f: 
+        f.write(local_key)
+    #encrypt our local key with our private key on a fernet instance
+    enc = Fernet(local_key).encrypt(private_key)
+
+    with open(KEYSTORE_KEY, 'wb') as f: 
+        f.write(enc)
+
+else:
+    local_key = open(KEYSTORE_KEY, 'rb').read()
+    enc = open(KEYSTORE_FILE, 'rb').read()
+    private_key = Fernet(local_key).decrypt(enc)
+
+print(f"ML-KEM: {ALG} loaded")
+
+
+#csv for data storage to check information: 
+with open(CSV_FILE, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["Iteration", "EncapTime_s", "DecapTime_s", "CPU%", "MemBytes"])
+
+
+# benchmarking: 
+print("Begining benchmark: ")
+
+for i in range(ITERATION):
+    try: 
+        proc = psutil.Process()
+
+        # fetch server public key
+        srv_pub = requests.get(f"{SERVER}/public_key", verify=False).json()
+        server_pk = b64_d(srv_pub["public_key_b64"])
+        # generate client keypair
+        with oqs.KeyEncapsulation(ALG) as kem_client:
+            client_pk = kem_client.generate_keypair()
+
+        # record start time for encapsulation
+        t_enc_start = time.perf_counter()
+
+        with oqs.KeyEncapsulation(ALG) as kem_enc:
+            kem_ciphertext, shared_secret = kem_enc.encap_secret(server_pk)
+
+        #end time for encapsualtion
+        t_enc_end = time.perf_counter()
+
+
+        #this part makes no sense to me i ripped this off github and then a gpt hallucination
+        #create the simulated message for symmetric encryption: 
+        hkdf = HKDF(algorithm=hashes.SHA256(), length=44, salt=None, info=b"fips203-packet-v1")
+        okm = hkdf.derive(shared_secret)
+        key, nonce = okm[:32], okm[32:44]
+        aead = ChaCha20Poly1305(key)
+        aad = b"hdr:demo"
+        ct = aead.encrypt(nonce, b"Benchmarking packet", aad)
+
+        #decryption timer 
+        t_decrypt_start = time.perf_counter()
+
+        with oqs.KeyEncapsulation(ALG) as kem_decap: 
+            shared_secret_client = kem_decap.decap_secret(kem_ciphertext, private_key)
+
+        t_decrypt_end = time.perf_counter()
+        
+
+
+    except Exception as e: 
+        print(f"u messed up, heres your mess up: \n{e}")
+        continue
--- server_app.py
+++ server_app.py
@@ -2,4 +2,5 @@
     this is the server app that will run on the ec2 instance. 
     packets will be encrypted with the public key recieved from the client and then sent back to the client.
     performance metrics will also be sent back along with the encrypted data
+    (maybe this can run on like sockets if we try but i dont really want to)
 '''
