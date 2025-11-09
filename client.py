e1f24be6693da7a8b67a07a854763d24e9672785
josssss07 <joshuadesai03@gmail.com>
2025-11-08 19:27:49 -0500

    configs added

--- client.py
+++ client.py
@@ -1,6 +1,25 @@
 '''
-    client.py will run on the client device, this will generate the public and private key pair. 
-    the public key will be sent to the aws ec2 isntance 
-    the performance metrics will be stored in a csv file and then the average will be taken. 
-    this can be compared against traditional algorithms like rsa 
+client side code
 '''
+
+#imports: 
+import base64, os, time, csv, psutil
+import requests, statistics
+from  cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
+import oqs 
+from cryptography.fernet import Fernet
+import warnings
+
+
+#ignore self signed HTTPS warnings: 
+warnings.filterwarnings("ignore")
+
+#configs 
+SERVER = "https://aws_goes_here_later:5000" #fips203 will run on port 5000 and rsa will run on port 5001 (read info.txt)
+ITERATION = 100 #api will be called 100 times when program is run to store data into our csv file. can be changed accordingly 
+CSV_FILE = 'benchnmark_results.csv' 
+
+
+
